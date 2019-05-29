a844c45f0aa8b9fc7a675e82c17ad69f7a86c61f
Jason Le <jas0nle@yahoo.com>
2019-05-29 20:05:13 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -19,11 +19,12 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(n) {
+  var i = 0
   function incrementVariable() {
     i = i + 1;
     return i;
   }
   do {
     console.log("I run once regardless.");
-  } while (incrementVariable() < n)
+  } while (incrementVariable() < n);
 }
